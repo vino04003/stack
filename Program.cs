1aef72c78349f7df108532f488f31d4cb5d552df
vino04003 <89254939+vino04003@users.noreply.github.com>
2023-01-26 12:08:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string containing brackets:");
+        string input = Console.ReadLine();
+
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in input)
+        {
+            if (c == '(' || c == '[' || c == '{')
+            {
+                stack.Push(c);
+            }
+            else if (c == ')' || c == ']' || c == '}')
+            {
+                if (stack.Count == 0)
+                {
+                    Console.WriteLine("Unbalanced");
+                    return;
+                }
+
+                char last = stack.Pop();
+                if ((c == ')' && last != '(') || (c == ']' && last != '[') || (c == '}' && last != '{'))
+                {
+                    Console.WriteLine("Unbalanced");
+                    return;
+                }
+            }
+        }
+
+        if (stack.Count == 0)
+        {
+            Console.WriteLine("Balanced");
+        }
+        else
+        {
+            Console.WriteLine("Unbalanced");
+        }
+    }
+}
